97017c4acb31fc85d584aedb93188531e23619c7
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-13 15:22:25 -0500

    create method to figure out if you need item or not

--- exercise6.rb
+++ exercise6.rb
@@ -20,3 +20,17 @@ add_item(groceries)
 
 #Output the total number of items on your list
 puts "The total number of items on your list is: #{groceries.length}"
+
+#Check to see if your list includes any item and tell the user to pick up if it does
+def check_list (list, item)
+  if list.include?(item) == true
+    puts "You need to pick up " + item
+  else
+    puts "You do not need to pick up " + item
+  end
+end
+
+check_list(groceries, "banana")
+
+#display second item on the list
+puts groceries[1]
