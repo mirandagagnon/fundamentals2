5eb3bfaae488b6640ad6f0944f253072205476ce
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-13 14:59:19 -0500

    Output number of items in list

--- exercise6.rb
+++ exercise6.rb
@@ -2,6 +2,21 @@
 groceries = ["apple", "banana", "eggs", "cheese"]
 
 #Output array in a list with an * in front of each item
-groceries.each do |items|
-  puts "* " + items
+# groceries.each do |items|
+#   puts "* " + items
+# end
+
+#add item to array
+groceries << "rice"
+
+#Create a method to list items in grocery list
+def add_item (list)
+  list.each do |item|
+    puts "* " + item
+  end
 end
+
+add_item(groceries)
+
+#Output the total number of items on your list
+puts "The total number of items on your list is: #{groceries.length}"
