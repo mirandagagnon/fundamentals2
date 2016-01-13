333200e977e1ce12d657f23027706126ad72ee13
Miranda Gagnon <miranda.kay.gagnon@gmail.com>
2016-01-13 14:35:49 -0500

    complete exercise 5

--- exercise5.rb
+++ exercise5.rb
@@ -1,13 +1,16 @@
 #create method to do the calculation
+
 def convert (f_temp)
   (f_temp - 32) * 5/9
 end
 
 #ask user to enter temperature and save result as variable
+
 puts "Please enter the temperature you would like to convert:"
 user_f_temp = gets.chomp.to_i
 
-# #call method and pass in user answer
+#call method and pass in user answer
+
 c_temp = convert(user_f_temp)
 
 # Output answer to user
